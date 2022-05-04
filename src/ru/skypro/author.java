d627b5827c06992f036c22580aa36996592117ed
ArtemCH240322 <sun2383@mail.ru>
2022-05-04 23:34:50 +0300

    Первый комит. 1

---
+++ src/ru/skypro/author.java
@@ -0,0 +1,17 @@
+package ru.skypro;
+
+public class author {
+    public String authorFirstName;
+    public String authorSecondName;
+
+    public Author(String authorFirstName, String authorSecondName) {
+        this.authorFirstName = authorFirstName;
+        this.authorSecondName = authorSecondName;
+    }
+
+    public String getAuthorName() {return authorFirstName + authorSecondName;}
+
+    public String getAuthorFirstName() {return authorFirstName;}
+
+    public String getAuthorSecondName() {return  authorSecondName;}
+}
