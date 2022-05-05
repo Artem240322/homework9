c983a5d8c91207e20fa0c21eb682418c21713dc3
ArtemCH240322 <sun2383@mail.ru>
2022-05-05 16:43:19 +0300

    Первый комит. 1,2 правка  + Дз. Методы обьектов

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -11,5 +11,24 @@ public class Main {
     System.out.println("Год издания стал =" + firstBook.getPublishingYear());
     System.out.println(firstBook.getBookName() + " " + firstBook.getAuthorName().getAuthorName() + " " + firstBook.getPublishingYear());
 
+    author firstAuthor_1 = new author("Имя", "Фамилия");
+    System.out.println("Автор книги " + firstAuthor.getAuthorFirstName() + firstAuthor.getAuthorSecondName());
+    Book firstBook_1 = new Book("Книга с названием", firstAuthor,1950);
+    System.out.println("Год издания был = " + firstBook.getPublishingYear());
+    firstBook_1.getPublishingYear();
+    System.out.println("Год издания стал =" + firstBook.getPublishingYear());
+    System.out.println(firstBook.getBookName() + " " + firstBook.getAuthorName().getAuthorName() + " " + firstBook.getPublishingYear());
+
+    System.out.println(firstAuthor.toString());
+    System.out.println(firstBook.toString());
+
+    System.out.println("Одинаковый автор? " + firstAuthor.equals(firstAuthor_1));
+    System.out.println("Одинаковая книга? " + firstBook.equals(firstBook_1));
+
+    firstBook.setPublishingYear(1955);
+
+    System.out.println(firstBook.hashCod());
+    System.out.println(firstBook_1.hashCod());
+
     }
 }
--- src/ru/skypro/author.java
+++ src/ru/skypro/author.java
@@ -14,4 +14,26 @@ public class author {
     public String getAuthorFirstName() {return authorFirstName;}
 
     public String getAuthorSecondName() {return  authorSecondName;}
+
+
+    /// ДЗ Методы обьектов
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + authorFirstName + '\'' +
+                ", subname='" + authorSecondName + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        author author = (author) o;
+        return authorFirstName.equals(author.authorFirstName) && authorSecondName.equals(authorSecondName);
+    }
+    @Override
+    public int hashCode() {return Object.hash(authorFirstName, authorSecondName);
+
+    }
 }
