e706f3755d41f5a81173f458c810733f7d2d3dae
ArtemCH240322 <sun2383@mail.ru>
2022-05-07 00:14:58 +0300

    Первый комит. 1,2 правка  + Дз. Методы обьектов. Ой !!! все сработало/ Приват прописал - но, что то изменений не вижу. Доп правки 8

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,17 +6,17 @@ public class Main {
         Author firstAuthor = new Author("Имя", "Фамилия");
         System.out.println("Автор книги " + firstAuthor.getAuthorFirstName() + firstAuthor.getAuthorSecondName());
         Book firstBook = new Book("Книга с названием", firstAuthor, 1950);
-        System.out.println("Год издания был = " + firstBook.getPublishingYear());
-        firstBook.getPublishingYear();
-        System.out.println("Год издания стал =" + firstBook.getPublishingYear());
+        System.out.println("Год издания был= " + firstBook.getPublishingYear());
+        firstBook.setPublishingYear(1955);
+        System.out.println("Год издания стал=" + firstBook.getPublishingYear());
         System.out.println(firstBook.getBookName() + " " + firstBook.getAuthorName().getAuthorName() + " " + firstBook.getPublishingYear());
 
         Author firstAuthor_1 = new Author("Имя ", " Фамилия");
         System.out.println("Автор книги " + firstAuthor.getAuthorFirstName() + firstAuthor.getAuthorSecondName());
         Book firstBook_1 = new Book("Книга с названием", firstAuthor, 1950);
-        System.out.println("Год издания был = " + firstBook.getPublishingYear());
-        firstBook_1.getPublishingYear();
-        System.out.println("Год издания стал =" + firstBook.getPublishingYear());
+        System.out.println("Год издания был=" + firstBook.getPublishingYear());
+        firstBook_1.setPublishingYear(1955);
+        System.out.println("Год издания стал=" + firstBook.getPublishingYear());
         System.out.println(firstBook.getBookName() + " " + firstBook.getAuthorName().getAuthorName() + " " + firstBook.getPublishingYear());
 
         System.out.println(firstAuthor.toString());
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Одинаковый автор? " + firstAuthor.equals(firstAuthor_1));
         System.out.println("Одинаковая книга? " + firstBook.equals(firstBook_1));
 
-        firstBook.setPublishingYear(1955);
+        firstBook.setPublishingYear(1975);
 
         System.out.println(firstBook.hashCode());
         System.out.println(firstBook_1.hashCode());
